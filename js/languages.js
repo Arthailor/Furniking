28fc13b3518be3aa62de47751fb379004b2afd6f
Arthailor <denart020@gmail.com>
2024-05-12 15:43:29 +0300

    Add dark theme

---
+++ js/languages.js
@@ -0,0 +1,26 @@
+const langArr = {
+    "welcome" :  {
+        "ru": "Добро пожаловать в наш онлайн магазин",
+        "en": "Welcome to our online shop",
+    }, 
+    "theme": {
+        "ru": "Поменять тему",
+        "en": "Switch theme",
+    }, 
+    "or": {
+        "ru": "или",
+        "en": "or",
+    }, 
+    "login": {
+        "ru": "Войти",
+        "en": "Login",
+    }, 
+    "signup": {
+        "ru": "Регистрация",
+        "en": "Sign up",
+    },  
+    "or": {
+        "ru": "или",
+        "en": "or",
+    }, 
+}
